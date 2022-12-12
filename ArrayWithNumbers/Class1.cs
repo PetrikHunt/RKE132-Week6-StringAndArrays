396005615d124bd9abc8c50a2bab6786756c994f
PetrikHunt <petrikhuntofficial@gmail.com>
2022-12-12 18:50:49 +0200

    create new app

---
+++ ArrayWithNumbers/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ArrayWithNumbers
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snack = { "sushi", "pizza", "burger", "chicken wings", "Asian takeaway" };
+
+Random random = new Random();
+ int randomIndex = random.Next(0, snack.Length);
+
+Console.WriteLine(snack[randomIndex]);
+
